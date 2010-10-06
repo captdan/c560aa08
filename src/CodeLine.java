b64d0fe86a48c405addadc282ee900aa23b1beed
kermitstearns <kermitstearns@858f0e9a-7517-a493-461e-9f87f100825c>
2010-10-06 20:58:00 +0000

    Added a "hasDirective" method in our CodeLine class so we can quickly see if there are any directives in a CodeLine.

--- src/CodeLine.java
+++ src/CodeLine.java
@@ -1,10 +1,11 @@
 import java.util.ArrayList;
+import java.util.HashSet;
 
 //CodeLine.java
 class CodeLine 
 {
 	// Define Variables
-	public String originalLineOfCode = "";
+	private String originalLineOfCode = "";
 	public String comment = "";
 	public ArrayList<Operand> operands = new ArrayList<Operand>();
 	public Instruction instruction = new Instruction();
@@ -13,6 +14,38 @@ class CodeLine
 	// Constructor
 	CodeLine(String lineOfCode) 
 	{
-		originalLineOfCode = lineOfCode;
+		this.originalLineOfCode = lineOfCode;
+	}
+	
+	public boolean hasDirective ()
+	{
+		HashSet<String> directives = new HashSet<String>();
+		directives.add(".START");
+		directives.add(".END");
+		directives.add(".DATA");
+		directives.add(".TEXT");
+		directives.add("INT.DATA");
+		directives.add("STR.DATA");
+		directives.add("HEX.DATA");
+		directives.add("BIN.DATA");
+		directives.add("ADR.DATA");
+		directives.add("ADR.EXP");
+		directives.add("ENT");
+		directives.add("EXT");
+		directives.add("NOP");
+		directives.add("EXEC.START");
+		directives.add("MEM.SKIP");
+		directives.add("EQU");
+		directives.add("EQU.EXP");
+		directives.add("RESET.LC");
+		directives.add("DEBUG");
+		
+		String copy = this.originalLineOfCode;
+		String [] splitCopy = copy.split("");
+		if (directives.contains(splitCopy[0]))
+		{
+			return true;
+		}
+		return false;
 	}
 }
