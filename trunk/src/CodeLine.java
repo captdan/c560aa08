7219e3ba052e3320838d2d2fcd1f5dbc4163cb3b
kermitstearns <kermitstearns@858f0e9a-7517-a493-461e-9f87f100825c>
2010-10-09 22:49:30 +0000

    -added Integer field "length" to CodeLine objects, so after the line is processed and we know what it is, the calculated length will be put in this field, and then in Parser, this field will be read and added to the global PC variable

--- trunk/src/CodeLine.java
+++ trunk/src/CodeLine.java
@@ -10,6 +10,12 @@ class CodeLine
 	public Instruction instruction = null;
 	public Directive directive = null;
 	public ArrayList<Error> errors = new ArrayList<Error>();
+	
+	/**
+	 * This will be the field where each line of code's length is stored. 
+	 * This field is then read from each CodeLine object in our Main Parser and added to the global Program Counter.
+	 */
+	public static Integer length;
 
 	// Constructor
 	CodeLine(String lineOfCode) 
