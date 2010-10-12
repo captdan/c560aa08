c5cd7443e8e842f3b219206fc59611713a2dafcb
gunhaver20x6 <gunhaver20x6@858f0e9a-7517-a493-461e-9f87f100825c>
2010-10-12 06:21:51 +0000

    added javadoc comments

--- trunk/src/LiteralTable.java
+++ trunk/src/LiteralTable.java
@@ -1,22 +1,37 @@
 import java.util.ArrayList;
 
+/**
+ * A class to hold all of the literals encountered.
+ * Literals are added along with the address at which they occur.
+ * A list of String[] containing the literals and their addresses can be obtained.
+ * @author danielburnett
+ *
+ */
 public class LiteralTable {
-	public ArrayList<String[]> table = new ArrayList<String[]>();
-	//public ArrayList<String> literals = new ArrayList<String>();
-	//public ArrayList<String> addresses = new ArrayList<String>();
-	
-	public void AddLiteral (String literal, String addr)
-	{
+	/**
+	 * Holds all of the literals and their addresses.
+	 */
+	private ArrayList<String[]> table = new ArrayList<String[]>();
+
+	/**
+	 * Allows a literal along with its corresponding address to be added to the table.
+	 * @param literal
+	 * 			The literal to be added.
+	 * @param addr
+	 * 			The address of the literal being added.
+	 */
+	public final void addLiteral(String literal, String addr) {
 		String[] temp = new String[2];
 		temp[0] = literal;
 		temp[1] = addr;
 		table.add(temp);
 	}
-	
-	public ArrayList<String[]> GetLiteralList ()
-	{
+
+	/**
+	 * Returns a list of the literals and their addresses.
+	 * @return The list to be returned.
+	 */
+	public final ArrayList<String[]> getLiteralList() {
 		return table;
 	}
-	
-	
 }
