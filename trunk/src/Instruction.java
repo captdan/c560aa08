0e5e68836df81847f538c6a081923c508e9e6f99
robertjmschmidt@gmail.com <robertjmschmidt@gmail.com@858f0e9a-7517-a493-461e-9f87f100825c>
2010-10-31 03:58:59 +0000

    Added ability to process R type files down to their hex code line

--- trunk/src/Instruction.java
+++ trunk/src/Instruction.java
@@ -64,12 +64,82 @@ public class Instruction
 		{
 			this.instructionExtendedBinary = String.valueOf(Integer.toBinaryString(Integer.parseInt(instructionExtendedHexValue.trim(), 16)));	
 		}
-
+	}
+	public String returnBinaryCodeLine()
+	{
+		String hexCodeLine = "";
+		//R type instructions
+		if(this.instructionType == Instruction.instructionTypes.REGISTER)
+		{
+			String shiftString = "";
+			if(this.instruction.equals("SRL") || this.instruction.equals("SLL") || this.instruction.equals("SRA"))
+			{
+				shiftString = String.valueOf(Integer.toBinaryString(Integer.parseInt(operandsArray.get(2).operand)));
+				operandsArray.get(2).operand = "";
+			}
+			
+			hexCodeLine += padZeros(this.instructionBinary,6);			//Opcode 	6 bits
+			hexCodeLine += padZeros("",2); 								//Unused 	2 bits
+			hexCodeLine += returnRegisterBinary(returnOperand(0)); 		//Reg 1 	3 bits
+			hexCodeLine += returnRegisterBinary(returnOperand(1)); 		//Reg 2 	3 bits
+			hexCodeLine += returnRegisterBinary(returnOperand(2)); 		//Reg 3 	3 bits
+			hexCodeLine += padZeros(shiftString,6); 					//Shift 	6 bits
+			hexCodeLine += padZeros("",3); 								//Unused 	2 bits
+			hexCodeLine += padZeros(this.instructionExtendedBinary,6);	//Function 	2 bits
+		}
+		
+		
+		return hexCodeLine;
+	}
+	public String returnHexCodeLine()
+	{
+		String binaryString = returnBinaryCodeLine();
+		String hexString = "";
+		for(int x = 0;x<(binaryString.length() / 4);x++)
+		{
+			//System.out.println(binaryString.substring((4*x), (4*x)+4));
+			hexString += Integer.toHexString(Integer.valueOf(binaryString.substring((4*x), (4*x)+4), 2));
+		}
+		return hexString;
 
 	}
 	public Instruction()
 	{
 	
+	}
+	public static String padZeros(String stringValue,int StringLength)
+	{
+		String newString = "";
+		for(int x=0;x<(StringLength-stringValue.length());x++)
+		{
+			newString += "0";
+		}
+		newString += stringValue;
+		return newString;
+	}
+	public static String returnRegisterBinary(String registerString)
+	{
+		int registerNumber = 0;
+		if (registerString.length() == 2)
+		{
+			registerNumber = Integer.parseInt(Character.toString(registerString.charAt(1)));
+		}
+		return padZeros(String.valueOf(Integer.toBinaryString(registerNumber)),3);
+		
+	}
+	public String returnOperand(int operandIndex)
+	{
+		String operandString = "";
+		if(operandIndex < operandsArray.size())
+		{
+			operandString = operandsArray.get(operandIndex).operand;
+		}
+		else
+		{
+			operandString = "";
+		}
+		
+		return operandString;
 	}
 	
 }
