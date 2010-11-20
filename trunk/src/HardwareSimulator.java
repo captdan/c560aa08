831e560f2372d7b126f193bc263b88586581c00a
shakaxx89@gmail.com <shakaxx89@gmail.com@858f0e9a-7517-a493-461e-9f87f100825c>
2010-11-20 20:30:28 +0000

    OK, So i think I might've done too much, I handled LH and LE together with LT because it was easy, and I let the dump function to print the MEM array from the first address to which we load to the last address +32 ... like the sparc Dump .... But I have a Ton of questions, can we maybe set up a meeting with Bri this Sunday?

--- trunk/src/HardwareSimulator.java
+++ trunk/src/HardwareSimulator.java
@@ -10,7 +10,7 @@ import java.util.StringTokenizer;
 public class HardwareSimulator {
 
 	int[] registers = new int[15];
-	static ArrayList<String> MEM = new ArrayList<String>();
+	static String[] MEM = new String[65536];
 	static ArrayList<String> LoadModule = new ArrayList<String>();
 	String opcode = "";
 	ArrayList<Error> Errors = new  ArrayList<Error>();
@@ -19,6 +19,10 @@ public class HardwareSimulator {
 	static String binaryInstruction = "";
 	public static ArrayList<String> GSymbTableArray = new ArrayList<String>();
 	public static GlobalSymbolTable GSymbTable = new GlobalSymbolTable();
+	public static int exectStart = 0;
+	public static int initialLoadAddr = 0;
+	public static int CompleteModuleLength = 0;
+	public static String LHfirstModuleName;
 	public static enum instructionType
 	{
 		 R, S, J, IO;
@@ -33,46 +37,41 @@ public class HardwareSimulator {
 	public static void main(String[] args) {
 		// Input Global Symbol Table from .txt into SymbolTable-type object
 		
-		// TODO Input our Load Module as .txt and generate memory array OSCAR
+		inisializeMEM();
 		instructionType type;
-		DumpArray();
-		if(args.length == 2 )
-		{
+	//	DumpArray();
+	//	if(args.length == 2 )
+	//	{
 		LoadModule = readFileToArrayList(args[0]);
-		GSymbTableArray = readFileToArrayList(args[1]);
-		}
-		else{
-			System.out.println("Unspecified Load File name");
-		}
+		//System.out.println(LoadModule.size());
+		
+	//	GSymbTableArray = readFileToArrayList(args[1]);
+	//	}
+	//	else{
+	//		System.out.println("Unspecified Load File name");
+	//		System.exit(2);
+	//	}
 		
 		/*
 		 * get Symbol table array into GSymbTable 
 		 * 
 		 */
-		for(int i =0; i<GSymbTableArray.size(); i++){
-			String Line = GSymbTableArray.get(i);
-			StringTokenizer st = new StringTokenizer(Line, " \t |");
-			if(st.countTokens() == 4){
-				String label = st.nextToken();
-				String assemblerAddr = st.nextToken(); 
-				String LinkerAddr = st.nextToken();
-				String LengthOfModule = st.nextToken();
-				GSymbTable.addSymbol( label,  assemblerAddr,  LinkerAddr,  LengthOfModule);
-			}
-			else{
-				System.out.println("Error: Missing element in global symbol table");
-			}
-			
-		}
+//		FillGlobalSymbTable();
+	
 		
-		// TODO Handle Linker Header Record as first object in LoadModule Array KERMIT
+		/*
+		 * Fill MEM array
+		 */
+		putToMEM();
+		
+		
 		
 		// TODO Handle Linker End Record as last object in LoadModule Array (Everything else is text records) RAKAAN
 		
 		// Load any int.data,bin.data,hex.data.str.data values into memory array
 		type = null;
 		
-			
+	/*		
 		for(int i = 0; i < Integer.MAX_VALUE; i++)
 		{
 			
@@ -96,8 +95,177 @@ public class HardwareSimulator {
 			
 			
 		}
-		
+		*/
 		DumpArray();
+	}
+
+	/**
+	 * Module Name:
+	 * Description:
+	 * Input Params:
+	 * Output Params:
+	 * Error Conditions Tested:
+	 * Error Messages Generated:
+	 * Original Author:
+	 * Date of Installation:
+	 * Modifications:
+	 */
+	private static void inisializeMEM() {
+		for(int i = 0; i < MEM.length; i++){
+			MEM[i] = "00000000";
+		}
+		
+	}
+
+	/**
+	 * Module Name:
+	 * Description:
+	 * Input Params:
+	 * Output Params:
+	 * Error Conditions Tested:
+	 * Error Messages Generated:
+	 * Original Author:
+	 * Date of Installation:
+	 * Modifications:
+	 */
+	private static void FillGlobalSymbTable() {
+		for(int i =0; i<GSymbTableArray.size(); i++){
+			String Line = GSymbTableArray.get(i);
+			StringTokenizer st = new StringTokenizer(Line, " \t |");
+			if(st.countTokens() == 4){
+				String label = st.nextToken();
+				String assemblerAddr = st.nextToken(); 
+				String LinkerAddr = st.nextToken();
+				String LengthOfModule = st.nextToken();
+				GSymbTable.addSymbol( label,  assemblerAddr,  LinkerAddr,  LengthOfModule);
+			}
+			else{
+				System.out.println("Error: Missing element in global symbol table");
+			}
+			
+		}
+		
+	}
+
+	/**
+	 * Module Name:
+	 * Description:
+	 * Input Params:
+	 * Output Params:
+	 * Error Conditions Tested:
+	 * Error Messages Generated:
+	 * Original Author:
+	 * Date of Installation:
+	 * Modifications:
+	 * @param loadModule2
+	 */
+	private static void putToMEM() {
+		try{
+			// TODO Handle Linker Header Record as first object in LoadModule Array KERMIT
+			// TODO Input our Load Module as .txt and generate memory array OSCAR//
+			String LH = LoadModule.get(0);
+			StringTokenizer LHTokenizer = new StringTokenizer(LH, "|");
+			
+			if(LHTokenizer.countTokens() == 11)
+				{
+				
+					String H = LHTokenizer.nextToken();
+					String exStart = LHTokenizer.nextToken();
+					exectStart = Integer.parseInt(exStart);
+					//System.out.println(exectStart);
+					LHfirstModuleName = LHTokenizer.nextToken();
+					String LengthOfModule = LHTokenizer.nextToken();
+					
+					CompleteModuleLength = Integer.parseInt(LengthOfModule);
+					//System.out.println(CompleteModuleLength);
+					String IniLodAddr = LHTokenizer.nextToken();
+					initialLoadAddr = Integer.parseInt(IniLodAddr);
+					PC = initialLoadAddr;
+					String Date = LHTokenizer.nextToken();
+					String Time = LHTokenizer.nextToken();
+					String SAL = LHTokenizer.nextToken();
+					String version = LHTokenizer.nextToken();
+					String revision = LHTokenizer.nextToken();
+					String firstModuleName2 = LHTokenizer.nextToken();
+				}
+			else
+				{
+					//TODO Generate Error .. missing arguments in LH
+				}
+			
+			String LT = LoadModule.get(1);
+			//System.out.println(LT);
+			StringTokenizer st = new StringTokenizer(LT, "|");
+			for(int i = 2; st.nextToken().equals("LT") ; i++)
+			{
+				
+				if(st.countTokens() == 4)
+				{
+					String loardAddr = st.nextToken();
+						if(((isValidHex(loardAddr)&& (loardAddr.length() == 4))) == false);
+						{
+								// TODO Generate Error .. invalid hex
+						}
+					// get Load address for MEM	
+					int loadAddsDecimal = Integer.parseInt(loardAddr, 16);
+					
+					String debug = st.nextToken();
+						if((debug.equals("Y") || (debug.equals("N"))) == false)
+						{
+							// TODO Generate error .. invalid debug flag
+						}
+					String Hex = st.nextToken();
+						if(((isValidHex(Hex)&& (Hex.length() == 8))) == false);
+						{
+						// TODO Generate Error .. invalid hex
+						}
+					String moduleName = st.nextToken();
+					// add the Hex code to MEM
+					try
+					{
+					MEM[loadAddsDecimal] = Hex;	
+					}
+					catch(Exception e)
+					{
+						// Generate Error ... MEM out of bounds
+					}
+					
+				}
+				else
+				{
+					// TODO GEnerate Error .. missing argument in Load File
+ 				}
+				// Increase PC
+				PC++;
+				LT = LoadModule.get(i);
+				st = new StringTokenizer(LT, "|");
+			}
+			/*
+			 * Handle LE
+			 */
+			String LE = LoadModule.get(LoadModule.size()-1);
+			StringTokenizer LETokenizer = new StringTokenizer(LE, "|");
+			
+			if(LHTokenizer.countTokens() == 3)
+				{
+				
+					String endRecord = LETokenizer.nextToken();
+					String totalNumberOfMudules = LETokenizer.nextToken();
+					String LEfirstModuleName = LETokenizer.nextToken();
+					if(LEfirstModuleName.equals(LHfirstModuleName) == false){
+						//TODO Generate Error ... miss-matching LH and LH firstModuleName 
+					}
+					
+				}
+			else
+			{
+				// TODO generate Error .... invalid number of arguments in LE
+			}
+		}
+		
+		catch(Exception e){
+			
+		}
 	}
 
 	private static void evaluateSType() 
@@ -151,10 +319,21 @@ public class HardwareSimulator {
 	private static void DumpArray()
 	{
 		// TODO Format Array OSCAR
+		System.out.println("Program MEM");
+		int last = CompleteModuleLength + initialLoadAddr +34;
 		
-		for(int i = 0; i < MEM.size() - 1; i++)
-		{
-			System.out.println(MEM.get(i));
+		int printcount = 0;
+		
+		for(int i = initialLoadAddr+1 ; i < last-1 ; i++){
+		
+				if(printcount % 8 == 0)
+				{
+					System.out.println();
+					System.out.print(i  + "\t");
+				}
+				System.out.print(MEM[i] + "  ");
+				printcount++;
+			
 		}
 	}
 	
@@ -167,6 +346,7 @@ public class HardwareSimulator {
 		// TODO Make a method. LATER
 	}
 	public static ArrayList<String> readFileToArrayList(String fileName)
+
 	{
 		ArrayList<String> linesOfCode = new ArrayList<String>();
 		try 
@@ -196,5 +376,18 @@ public class HardwareSimulator {
 			e.printStackTrace();
 		}
 		return linesOfCode;
+	}
+	static boolean isValidHex (String hex) 
+	{
+		//need to flesh this out, but each method to check operands should just return a boolean 
+		//max of 8 hex digits, fill in with signed value in front (either 0 or F)
+		boolean result = true;
+		for(int i =0; i <hex.length();i++){
+			if (!(Character.isDigit(hex.charAt(i))|| hex.charAt(i)== 'A' || hex.charAt(i)== 'B'|| hex.charAt(i)== 'C' || hex.charAt(i)== 'D' || hex.charAt(i)== 'E' || hex.charAt(i)== 'F')){
+				result = false;
+			}
+			
+		}
+		return (result);
 	}
 }
