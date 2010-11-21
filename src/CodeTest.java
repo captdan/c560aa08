9b6c57fabde3fbbe348535cc87c6d439aa40b82a
shakaxx89@gmail.com <shakaxx89@gmail.com@858f0e9a-7517-a493-461e-9f87f100825c>
2010-11-21 19:56:40 +0000

    ALU now has ADD,SUB,MUL,DIV,ADDU,SUBU,MULU,DIVU,PWR,AND,OR,ORI,XOR,NOR,XORI,NORI and ANDI .... I'm actually not sure if the logic of the immediate ones is correct ... but I can fix those in 5 minutes

---
+++ src/CodeTest.java
@@ -0,0 +1,14 @@
+
+public class CodeTest {
+public static void main(String[] args){
+	ALU A = new ALU();
+	String a = "1101001";
+	String b = "0001011";
+	String c = A.ANDI(a, b);
+	System.out.println(c);
+	
+
+//	System.out.println(d);
+	
+}
+}
