0b9a95f7958c5ea167edc85d6e1c44063b709652
rakaank2 <rakaank2@858f0e9a-7517-a493-461e-9f87f100825c>
2010-11-22 14:21:34 +0000

    IO handling in progress

--- trunk/src/HardwareSimulator.java
+++ trunk/src/HardwareSimulator.java
@@ -1,5 +1,6 @@
 import java.io.BufferedReader;
 import java.io.DataInputStream;
+import java.util.*;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
@@ -24,6 +25,7 @@ public class HardwareSimulator {
 	public static int CompleteModuleLength = 0;
 	public static String LHfirstModuleName;
 	public static ALU ALU = new ALU();
+	public static int TargetAddress = 0;
  	public static enum instructionType
 	{
 		 R, S, J, IO, I, NOTINSTRUCTION;
@@ -45,7 +47,7 @@ public class HardwareSimulator {
 	//	if(args.length == 2 )
 	//	{
 		//System.out.println(args[0]);
-		LoadModule = readFileToArrayList(args[0]);
+		//LoadModule = readFileToArrayList(args[0]);
 		//System.out.println(LoadModule.size());
 		
 	//	GSymbTableArray = readFileToArrayList(args[1]);
@@ -98,8 +100,19 @@ public class HardwareSimulator {
 					break;
 				case R:  evaluateRType(String.valueOf(OPCODE));
 					break;
-				case J:  evaluateJType(binaryInstruction);
-					break;
+				case J:  
+				DumpInfo();
+
+				if(debug)
+				{
+					DumpArray();
+				}
+				
+				PC++;
+				
+				System.exit(1);
+				
+				break;
 				case IO: evaluateIOType(binaryInstruction);
 					break;
 				default: Error();
@@ -141,11 +154,11 @@ public class HardwareSimulator {
 		//011010 
 		// At this point, OPCODE is the integer representation of the opcode
 		// when you do Integer.ParseInt ... it returns an integer not a hex number, so you need to compare to int not Hex
-		if((16 <= OPCODE) && (OPCODE <= 23) || (OPCODE >= 52) && (OPCODE <= 55) || (OPCODE == 61) || (OPCODE == 30) || (OPCODE == 31) )
+		if(((16 <= OPCODE) && (OPCODE <= 23))||((OPCODE <=50)&&(OPCODE <=55))|| (OPCODE ==61)||(OPCODE==30)||(OPCODE==31))
 		{
 			result = instructionType.I;
 		}
-		else if ((OPCODE >= 10) && (OPCODE <= 13))
+		else if ((10 <= OPCODE) && (OPCODE <= 13))
 		{
 			result = instructionType.IO;
 		}
@@ -153,19 +166,20 @@ public class HardwareSimulator {
 		{
 			result = instructionType.J;
 		}
-		else if (((OPCODE >= 1) && (OPCODE <= 3)) || ((OPCODE == 63)))
+		else if (((1 <= OPCODE) && (OPCODE <= 3)) ||(OPCODE==63))
 		{
 			result = instructionType.R;
 		}
-		else if(((OPCODE >= 32) && (OPCODE <= 36)) || (OPCODE == 39)|| (OPCODE == 48) || (OPCODE == 49) || ((OPCODE >= 56) && (OPCODE <= 59)) || (OPCODE == 6) || (OPCODE <= 7) || ((OPCODE >= 26) && (OPCODE <= 29)))
+		else 
 		{
 			result = instructionType.S;
 		}
-		else{
+
+		/*else{
 			result = instructionType.NOTINSTRUCTION;
 			//System.out.println("Hex Does not contain a valid instruction: Terminating");
 			//System.exit(1);
-		}
+		}*/
 		
 		return result;
 	}
@@ -382,7 +396,7 @@ public class HardwareSimulator {
 		PC++;
 	}
 
-	private static void evaluateJType(String binaryInstruction2) 
+	/*private static void evaluateJType(String binaryInstruction2) 
 	{
 		
 		DumpInfo();
@@ -394,12 +408,111 @@ public class HardwareSimulator {
 		PC++;
 		
 		DumpInfo();
-	}
+	}*/
 
-	private static void evaluateIOType(String binaryInstruction2) 
+	private static void evaluateIOType(String opcode) 
 	{
 		DumpInfo();
 		// TODO Evaluate Instruction RAKAAN
+		
+		if(opcode.equals("10"))
+		{
+			Scanner scan = new Scanner(System.in);
+			
+			int userInput = 0;
+			
+			System.out.print("Enter the number you want stored in memory:");
+			try{
+				
+				userInput = scan.nextInt();
+				
+			} catch (Exception e){
+				
+				//error reading user's input
+			}
+			
+			if (Integer.parseInt(binaryInstruction.substring(6,8))== 0)
+			{
+				//determine target address when relative to start
+			}
+			else if (Integer.parseInt(binaryInstruction.substring(6,8))== 1)
+			{
+				//determine target address when indirect relative to start
+			}
+			else if (Integer.parseInt(binaryInstruction.substring(6,8))== 2)
+			{
+				TargetAddress = Integer.parseInt(binaryInstruction.substring(16,32),2);
+			}
+			else if (Integer.parseInt(binaryInstruction.substring(6,8))== 3)
+			{
+				//determine target address when literal value is used.
+			}
+			
+			MEM[TargetAddress] = String.valueOf(userInput);
+			
+			
+		}
+		else if (opcode.equals("11"))
+		{
+			System.out.print("Enter the character string you want stored in memory:");
+			
+			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+			
+			String userInput = null;
+			
+			try
+			{
+				userInput = br.readLine();
+				
+			} catch(Exception e) {
+			
+				//error reading user's input				
+			}
+
+			if (Integer.parseInt(binaryInstruction.substring(6,8))== 0)
+			{
+				//determine target address when relative to start
+			}
+			else if (Integer.parseInt(binaryInstruction.substring(6,8))== 1)
+			{
+				//determine target address when indirect relative to start
+			}
+			else if (Integer.parseInt(binaryInstruction.substring(6,8))== 2)
+			{
+				TargetAddress = Integer.parseInt(binaryInstruction.substring(16,32),2);
+			}
+			else if (Integer.parseInt(binaryInstruction.substring(6,8))== 3)
+			{
+				//determine target address when literal value is used.
+			}
+			
+			MEM[TargetAddress] = userInput;
+			
+		}
+		else
+		{
+			if (Integer.parseInt(binaryInstruction.substring(6,8))== 0)
+			{
+				//determine target address when relative to start
+			}
+			else if (Integer.parseInt(binaryInstruction.substring(6,8))== 1)
+			{
+				//determine target address when indirect relative to start
+			}
+			else if (Integer.parseInt(binaryInstruction.substring(6,8))== 2)
+			{
+				TargetAddress = Integer.parseInt(binaryInstruction.substring(16,32),2);
+			}
+			else if (Integer.parseInt(binaryInstruction.substring(6,8))== 3)
+			{
+				//determine target address when literal value is used.
+			}
+			
+			//need to convert MEM[TargetAddress] to ASCII equivalent.
+			
+			//System.out.println(MEM[TargetAddress]);
+		}
+		
 		DumpInfo();
 		if(debug)
 		{
